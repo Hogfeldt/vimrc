3f5ff3c2901e0ad99375171d9dde31f6afb99bf1
Hogfeldt <hogfeldt38@gmail.com>
2018-10-04 20:25:58 +0200

    setup.sh will now check for dependencies, offer to install missing dependencies and create a sysmlink from ~/.vimrc to .vimrc

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 # vimrc
 
 #### Dependencies from the AUR repository:
++ vim
 + vim-airline
 + vim-airline-themes
 + vim-jellybeans
 + vim-latexsuite
 
-You can use setup.sh to see if dependencies are installed.
-
 #### Install guide:
-```
-git clone https://github.com/Hogfeldt/vimrc/
-ln -s ~/vimrc/.vimrc .vimrc
-```
++ Clone the repo
++ Execute the setup.sh
--- setup.sh
+++ setup.sh
@@ -1,14 +1,44 @@
 #!/bin/sh
-isPackageInstalled(){
+dependencies=(vim 
+vim-airline 
+vim-airline-themes 
+vim-jellybeans 
+vim-latexsuite)
+
+notInstalledPack=()
+checkIfPackageIsInstalled() {
 	if pacaur -Qi $1 > /dev/null ; then
 		echo "$1 is installed"
 	else
-		echo "$1 is not installed"
+		notInstalledPack+=("$1")
 	fi
 }
 
 echo "Checking for dependencies..."
-isPackageInstalled vim-airline
-isPackageInstalled vim-airline-themes
-isPackageInstalled vim-jellybeans
-isPackageInstalled vim-latexsuite
+for i in "${dependencies[@]}"
+do
+	checkIfPackageIsInstalled $i
+done 
+
+packStr=""
+for i in "${notInstalledPack[@]}"
+do
+	packStr="$packStr $i"
+done
+if [ "$packStr" != "" ] ; then
+	echo "The following packages is not installed:"
+	for i in "${notInstalledPack[@]}"
+	do
+		echo "- $i"
+	done
+	echo "Would you like them to be installed (Y)es og (N)o?"
+	select yn in "Yes" "No"; do
+		case $yn in
+			Yes ) pacaur -S "$packStr"; break;;
+			No ) exit;;
+		esac
+	done
+	path="$(pwd)/.vimrc"
+	ln -sf ~/.vimrc $path
+fi
+
